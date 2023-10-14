d8b6b67649eb239b1bb30700ac8f9bb26a4c03e3
Nguyễn Duy Khánh <109708814+duykhanh1632003@users.noreply.github.com>
2023-10-14 10:22:08 +0700

    first

---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,61 @@
+import { LogoTwitter } from '#build/components';
+import { useTailwindConfig } from './compatables/useTailwindConfig.vue';
+
+<template>
+        <div class="flex flex-col h-screen">
+            <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="defaultTransition">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
+        <div class="mt-2 space-y-3 w-8 ">
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                        <HomeIcon />
+                </template>
+                <template v-slot:name>
+                    Home
+                </template>
+                
+            </SidebarLeftTab>
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                        <HomeIcon />
+                </template>
+                <template v-slot:name>
+                    Home
+                </template>
+            
+            </SidebarLeftTab>
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                        <HomeIcon />
+                </template>
+                <template v-slot:name>
+                    Home
+                </template>
+            
+            </SidebarLeftTab>
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                        <HomeIcon />
+                </template>
+                <template v-slot:name>
+                    Home
+                </template>
+                
+            </SidebarLeftTab>
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+
+import { HomeIcon } from "@heroicons/vue/solid"
+
+const { defaultTransition } = useTailwindConfig()
+</script>
+
